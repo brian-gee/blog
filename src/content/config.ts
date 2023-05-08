2adb5b683638c9a0dbad4608513ed4587c98423b
Brian Geertsma <39035211+brian-gee@users.noreply.github.com>
2023-05-08 00:04:45 -0400

    reboot 2

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,21 @@
-import { defineCollection } from "astro:content";
-import { blogSchema } from "./_schemas";
+import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-  schema: blogSchema,
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z
+			.string()
+			.or(z.date())
+			.transform((val) => new Date(val)),
+		updatedDate: z
+			.string()
+			.optional()
+			.transform((str) => (str ? new Date(str) : undefined)),
+		heroImage: z.string().optional(),
+	}),
 });
 
 export const collections = { blog };
